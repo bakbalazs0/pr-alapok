9788f030b92c8d8f167ccee694bcaa18f7ef2e06
bakbalazs0 <124927048+bakbalazs0@users.noreply.github.com>
2023-09-28 10:08:10 +0200

    Add files via upload

---
+++ hazi_feladat/hazi_feladat_002.py
@@ -0,0 +1,8 @@
+#Házi feladat 002
+nap = input('Jó napod volt? ')
+if nap == 'Igen':
+    print('Az jó!')
+elif nap == 'Nem':
+    print('Az nem jó!')
+else: 
+    print('Sajnos nem értem a válaszodat!')
