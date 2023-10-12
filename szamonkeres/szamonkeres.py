b9559552f1dc16de4e5d250f085946dfe1431208
bakbalazs0 <124927048+bakbalazs0@users.noreply.github.com>
2023-10-12 09:28:05 +0200

    Add files via upload

---
+++ szamonkeres/szamonkeres.py
@@ -0,0 +1,11 @@
+# Bak Balázs Benjámin 10.D 1 csoport
+#szamonkeres_001.py
+"""
+Bekérek egy osztályzatot 1-5 irassuk ki a megadott jegyet értékkel szövegesen,
+ha nem megfelelő jegyet adtam akkor irassuk ki a jegy értékét és hogy nem megfelelő.
+"""
+osztalyzat = int(input("Kérek egy oszályzatot: "))
+if osztalyzat : 1
+print(f"A jegyed {osztalyzat}-es elégtelen.")
+elif osztalyzat : 2
+print(f"A jegyed {osztalyzat}-es elégséges.")
