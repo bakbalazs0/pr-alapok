f962e679a265bc200731334c1ac40cf4a11a0010
bakbalazs0 <124927048+bakbalazs0@users.noreply.github.com>
2023-12-06 23:18:42 +0100

    Delete hazi_feladat directory

--- hazi_feladat/hazi_feladat_001.py
+++
@@ -1,6 +0,0 @@
-#Házi feladat 001
-nap = input('Jó napod volt? ')
-if nap == 'Igen':
-    print('Az jó!')
-elif nap == 'Nem':
-    print('Az nem jó!')
--- hazi_feladat/hazi_feladat_002.py
+++
@@ -1,8 +0,0 @@
-#Házi feladat 002
-nap = input('Jó napod volt? ')
-if nap == 'Igen':
-    print('Az jó!')
-elif nap == 'Nem':
-    print('Az nem jó!')
-else: 
-    print('Sajnos nem értem a válaszodat!')
