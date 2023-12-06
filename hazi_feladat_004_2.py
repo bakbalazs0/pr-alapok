336540f90eff4f60d1bd17d1856d425fbb478e91
bakbalazs0 <124927048+bakbalazs0@users.noreply.github.com>
2023-12-06 23:25:51 +0100

    Delete hazi_feladat_004_2.py

--- hazi_feladat_004_2.py
+++
@@ -1,9 +0,0 @@
-#hazi_feladat_004/2
-#Fejleszd tovább az első feladat programját úgy, hogy amennyiben a felhasználó nem a két lehetséges válasz (igen/nem) közül ad meg egyet, a gép kiírja: "Sajnos nem értem a válaszodat!"
-nap = input('Jó napod volt? ')
-if nap == 'Igen':
-    print('Az jó!')
-elif nap == 'Nem':
-    print('Az nem jó!')
-else: 
-    print('Sajnos nem értem a válaszodat!')
