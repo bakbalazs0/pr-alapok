deadcd9f14a77c3c6244e4477d73a987a81f290e
bakbalazs0 <124927048+bakbalazs0@users.noreply.github.com>
2023-12-07 09:23:50 +0100

    Add files via upload

---
+++ feladat/feladat_014.py
@@ -0,0 +1,16 @@
+# while magyarul azt jelenti: amíg
+szam = 1
+while szam <= 10:
+    print(szam)
+    szam = szam + 1  
+
+print(f"A program vége")
+#----------------------------
+"""
+szamlalo = 1
+while szamlalo <= 5:
+    print('Programozni jó!')
+    szamlalo = szamlalo + 1      
+
+print(f"A program vége")
+"""
---
+++ feladat/feladat_019.py
@@ -0,0 +1,19 @@
+sor = 1 
+while sor <= 3:
+    oszlop = 1
+    while oszlop <= 5:
+        print("O", end ="")
+        oszlop = oszlop + 1
+    print("")
+    sor = sor + 1 
+#----------------------------
+""" 
+ sor = 1 
+while sor <= 3:
+    oszlop = 1
+    while oszlop <= 5:
+        print("O", end ="")
+        oszlop = oszlop + 1
+    print("")
+    sor = sor + 1 
+"""
